d048ce6ab35ecbb885cb5effa0534964425acbf3
Piyas <piyas@gmail.com>
2026-07-24 00:13:40 +0600

    init

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import compression from "compression";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(cors());
+app.use(compression());
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+// Default route for testing
+app.get("/", (_req, res) => {
+  res.send("API is running");
+});
+
+// 404 Handler
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: "Route Not Found",
+  });
+});
+
+export default app;
